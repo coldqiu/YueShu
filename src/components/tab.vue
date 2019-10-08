b01086f56236ad878da151a6f86766e5a783423b
coldqiu <gotta_blue@163.com>
2019-10-08 22:23:28 +0800

    start

--- src/components/tab.vue
+++ src/components/tab.vue
@@ -1,17 +1,22 @@
 <template>
   <div class="tab">
+    <router-link tag="div" class="tab-item" to="/timeline">
+      <span class="tab-link">timeline</span>
+    </router-link>
     <router-link tag="div" class="tab-item" to="/article">
-      <span class="tab-link">推荐</span>
+      <span class="tab-link">article</span>
     </router-link>
     <router-link tag="div" class="tab-item" to="/blog">
-      <span class="tab-link">歌手</span>
+      <span class="tab-link">blog</span>
     </router-link>
     <router-link tag="div" class="tab-item" to="/pic">
-      <span class="tab-link">排行
-      </span>
+      <span class="tab-link">pic</span>
     </router-link>
     <router-link tag="div" class="tab-item" to="/tag">
-      <span class="tab-link">搜索</span>
+      <span class="tab-link">tag</span>
+    </router-link>
+    <router-link tag="div" class="tab-item" to="/search">
+      <span class="tab-link">search</span>
     </router-link>
   </div>
 </template>
@@ -21,20 +26,21 @@ export default {
 }
 </script>
 
-<!--<style scoped lang="stylus" rel="stylesheet/stylus">-->
-  <!--.tab-->
-    <!--display: flex-->
-    <!--height: 44px-->
-    <!--line-height: 44px-->
-    <!--font-size: $font-size-medium-->
-    <!--.tab-item-->
-      <!--flex: 1-->
-      <!--text-align: center-->
-      <!--.tab-link-->
-        <!--padding-bottom: 5px-->
-        <!--color: $color-text-l-->
-      <!--&.router-link-active-->
-        <!--.tab-link-->
-          <!--color: $color-theme-->
-          <!--border-bottom: 2px solid $color-theme-->
-<!--</style>-->
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  .tab
+    display: flex
+    height: 44px
+    line-height: 44px
+    font-size: $font-size-medium
+    .tab-item
+      flex: 1
+      text-align: center
+      cursor: pointer
+      .tab-link
+        padding-bottom: 5px
+        color: $color-text-l
+      &.router-link-active
+        .tab-link
+          color: $color-theme
+          border-bottom: 2px solid $color-theme
+</style>
---
+++ src/pages/Search/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="wrap">
+    <h1>Search</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Search'
+}
+</script>
+
+<style scoped>
+
+</style>
